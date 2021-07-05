7201db861b565cd07c3796b7ce9a4246c43cb3a9
Tulio Gontijo <tulio_gontijo@hotmail.com>
2021-07-05 09:56:40 -0300

    Inclusao de CrossOrigin

--- src/main/java/com/controller/DoencasController.java
+++ src/main/java/com/controller/DoencasController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,27 +24,32 @@ public class DoencasController {
 
 	@Autowired
 	DoencasService service;
-
+	
+	@CrossOrigin
 	@GetMapping
 	public List<Doencas> recuperaTodosRegistros() {
 		return service.todos();
 	}
 
+	@CrossOrigin
 	@PostMapping
 	public Doencas criaNovoRegistro(@Valid @RequestBody Doencas doencas) {
 		return service.novo(doencas);
 	}
 
+	@CrossOrigin
 	@GetMapping("/{id}")
 	public Doencas buscaUmRegistro(@PathVariable Integer id) {
 		return service.busca(id);
 	}
 
+	@CrossOrigin
 	@PutMapping("/{id}")
 	public Doencas atualizaRegistro(@Valid @RequestBody Doencas doencas, @PathVariable Integer id) {
 		return service.atualiza(doencas, id);
 	}
 
+	@CrossOrigin
 	@DeleteMapping("/{id}")
 	void excluiRegistro(@PathVariable Integer id) {
 		service.exclui(id);
